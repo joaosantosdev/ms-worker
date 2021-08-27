cdb85a495b72a7f003801a8babeef218991ce6e5
João Santos <joaosantosdev@hotmail.com>
2021-08-27 20:38:59 -0300

    ribbon load balancing

--- human-resource-payroll/src/main/java/br/com/ourmind/hrpayroll/HumanResourcePayrollApplication.java
+++ human-resource-payroll/src/main/java/br/com/ourmind/hrpayroll/HumanResourcePayrollApplication.java
@@ -2,8 +2,10 @@ package br.com.ourmind.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "human-resource-worker")
 @EnableFeignClients
 @SpringBootApplication
 public class HumanResourcePayrollApplication {
--- human-resource-payroll/src/main/java/br/com/ourmind/hrpayroll/feignclients/WorkerFeignClient.java
+++ human-resource-payroll/src/main/java/br/com/ourmind/hrpayroll/feignclients/WorkerFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import br.com.ourmind.hrpayroll.entities.Worker;
 
 @Component
-@FeignClient(name = "human-resource-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "human-resource-worker", path = "/workers")
 public interface WorkerFeignClient {
 	
 	@GetMapping(value = "/{id}")
