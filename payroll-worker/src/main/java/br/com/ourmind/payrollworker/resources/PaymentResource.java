0a613baa60171e7978e742b376f5f61b9aa16d36
João Santos <joaosantosdev@hotmail.com>
2021-08-27 19:55:53 -0300

    communication with RestTemplate

--- payroll-worker/src/main/java/br/com/ourmind/payrollworker/resources/PaymentResource.java
+++ payroll-worker/src/main/java/br/com/ourmind/payrollworker/resources/PaymentResource.java
@@ -19,7 +19,7 @@ public class PaymentResource {
 	private PaymentService service;
 	
 	@GetMapping(value = "/{workerId}/days/{days}")
-	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
+	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) throws Exception{
 		return ResponseEntity.ok(this.service.getPayment(workerId, days));
 	}
 
