18c9a8e48d7646a99b564d609bd2d1c527db2d6b
João Santos <joaosantosdev@hotmail.com>
2021-08-26 22:38:52 -0300

    initial config

---
+++ human-resource-worker/src/main/java/br/com/ourmind/hrworker/HumanResourceWorkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ourmind.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceWorkerApplication.class, args);
+	}
+
+}
