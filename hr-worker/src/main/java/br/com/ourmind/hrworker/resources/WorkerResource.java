3719fab426399814fea8893b64f70729dcd6bc4e
João Santos <joaosantosdev@hotmail.com>
2021-08-28 09:24:46 -0300

    Actuator for runtime config refresh

--- hr-worker/src/main/java/br/com/ourmind/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/ourmind/hrworker/resources/WorkerResource.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.ourmind.hrworker.entities.Worker;
 import br.com.ourmind.hrworker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
@@ -24,7 +26,7 @@ public class WorkerResource {
 
 	@Value("${test.config}")
 	private String config;
-	
+
 	@Autowired
 	private Environment env;
 
@@ -44,7 +46,7 @@ public class WorkerResource {
 
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs() {
-		log.info("CONFIG: "+ this.config);
+		log.info("CONFIG: " + this.config);
 		return ResponseEntity.noContent().build();
 	}
 
