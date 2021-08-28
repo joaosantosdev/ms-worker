8f8c303ad219c0a0f3da3d41593282b0a21d3acb
João Santos <joaosantosdev@hotmail.com>
2021-08-27 22:04:00 -0300

    configuring hystrix

--- human-resource-payroll/src/main/java/br/com/ourmind/hrpayroll/resources/PaymentResource.java
+++ human-resource-payroll/src/main/java/br/com/ourmind/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.ourmind.hrpayroll.entities.Payment;
 import br.com.ourmind.hrpayroll.services.PaymentService;
 
@@ -18,9 +20,14 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentFallback")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) throws Exception{
 		return ResponseEntity.ok(this.service.getPayment(workerId, days));
+	}
+	
+	public ResponseEntity<Payment> getPaymentFallback(Long workerId, Integer days) throws Exception{
+		return ResponseEntity.ok(new Payment("Fallback", 400.0, days));
 	}
 
 }
