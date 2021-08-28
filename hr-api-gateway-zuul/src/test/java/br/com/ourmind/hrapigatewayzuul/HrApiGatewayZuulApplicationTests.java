4a5d2d38d024890a12bdf00e2fd95a5c4e356621
João Santos <joaosantosdev@hotmail.com>
2021-08-27 22:36:01 -0300

    configuring zull api gateway

---
+++ hr-api-gateway-zuul/src/test/java/br/com/ourmind/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ourmind.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
